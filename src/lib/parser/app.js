e2958fa6489cbff1f159516899d91174817b815c
ZhukDima <zhukdo@gmail.com>
2020-07-10 18:37:39 +0200

    Tests work, but they are not standardized.

--- src/lib/parser/app.js
+++ src/lib/parser/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const sum = (a, b) => a + b
 const sub = (a, b) => a - b
 const mul = (a, b) => a * b
--- src/tests/lib/parser/test.js
+++ src/tests/lib/parser/test.js
@@ -1,13 +1,28 @@
 import { Parser } from '../../../lib/parser/app.js';
 
-const test = (str) => { console.log(`${str} => ${new Parser(str).run()}`); }
+const test = str => { new Parser(str).run() }
 
-test(`1`)
-test(`"asdf"`)
-test(`12bc`)
-test(`="abc"+"def"`)
-test(`=(1+5^(1/2))/2`)
-test(`=(1-5^(1/2))/2`)
-test(`=СУММА(A2:F2;B3:ZA3)`)
-test(`=A1`)
-test(`=1+"b"+3+"c"`)
+const checkAns = (str, ans) => test(str) == ans
+
+const checkManyAns = strs => {
+    for (let x = 0; x < strs.length; x++) {
+        if (!checkAns(strs[x][0], strs[x][1])) {
+            console.log("Find miss!")
+            console.log(`True ans: ${strs[x][1]} \nFind ans: ${test(strs[x][0])}`)
+            return;
+        }
+    }
+    console.log("All true!")
+}
+
+checkManyAns([
+    [`1`, `1`],
+    [`"asdf"`, `"asdf"`],
+    [`12bc`, `12bc`],
+    [`="abc"+"def"`, `abcdef`],
+    [`=(1+5^(1/2))/2`, (1 + Math.sqrt(5)) / 2],
+    [`=(1-5^(1/2))/2`, (1 - Math.sqrt(5)) / 2],
+    [`=СУММА(A2:F2;B3:ZA3)`, [`СУММА`, [[[0, 2], [5, 2]], [[2, 3], [550, 3]]]]],
+    [`=A1`, [0, 1]],
+    [`=1+"b"+3+"c"`, `1b3c`],
+])
