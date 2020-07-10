cc01246912270bb1d1639a5a88d7fa35d19800b7
Артём Белоусов <flygrounder@yandex.ru>
2020-07-10 11:04:12 +0300

    Added Cell class

---
+++ src/lib/spreadsheets/Cell.js
@@ -0,0 +1,79 @@
+export const defaultCellColor = '#ffffff';
+export const valueTypes = {
+  number: 'number',
+  boolean: 'boolean',
+  string: 'string',
+  formula: 'formula',
+};
+
+export class Cell {
+  constructor(
+    type = valueTypes.string,
+    value = null,
+    color = null,
+  ) {
+    this.setValue(type, value);
+    this.setColor(color);
+  }
+
+  getProcessedValue() {
+    return this.value; // TODO Реализовать обработку формул
+  }
+
+  setValue(type, value = null) {
+    if (value === null) {
+      this.value = Cell.getDefaultValue(type);
+      this.type = type;
+    } else if (Cell.isValueCorrect(type, value)) {
+      this.type = type;
+      this.value = value;
+    } else {
+      throw new Error('value type does not correspond to specified cell type');
+    }
+  }
+
+  setColor(color = null) {
+    if (color === null) {
+      this.color = defaultCellColor;
+    } else if (Cell.isColorCorrect(color)) {
+      this.color = color;
+    } else {
+      throw new Error('specified color is not a correct hexadecimal color');
+    }
+  }
+
+  static isColorCorrect(color) {
+    const colorRegExp = RegExp(
+      '^#[0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f]$',
+    );
+    return colorRegExp.test(color);
+  }
+
+  static isValueCorrect(type, value) {
+    const currentTypeString = typeof value;
+    const requiredTypeString = Cell.getRequiredTypeString(type);
+    if (type === valueTypes.number
+      && (Number.isNaN(value) || !Number.isFinite(value))) {
+      return false;
+    }
+    return currentTypeString === requiredTypeString;
+  }
+
+  static getRequiredTypeString(type) {
+    return typeof Cell.getDefaultValue(type);
+  }
+
+  static getDefaultValue(type) {
+    switch (type) {
+      case valueTypes.number:
+        return 0;
+      case valueTypes.boolean:
+        return false;
+      case valueTypes.string:
+      case valueTypes.formula:
+        return '';
+      default:
+        throw new Error('unknown cell type');
+    }
+  }
+}
---
+++ src/tests/lib/spreadsheets/Cell.test.js
@@ -0,0 +1,94 @@
+import * as assert from 'assert';
+import {
+  valueTypes,
+  defaultCellColor,
+  Cell,
+} from '../../../lib/spreadsheets/Cell.js';
+
+describe('Cell', () => {
+  describe('#constructor()', () => {
+    it('should create an empty cell', () => {
+      const cell = new Cell();
+      assert.strictEqual(cell.type, valueTypes.string);
+      assert.strictEqual(cell.value, '');
+      assert.strictEqual(cell.color, defaultCellColor);
+    });
+    it('should create a cell with number', () => {
+      const cell = new Cell(valueTypes.number, 10);
+      assert.strictEqual(cell.type, valueTypes.number);
+      assert.strictEqual(cell.value, 10);
+    });
+    it('should throw an exception for 2 as boolean', () => {
+      assert.throws(() => {
+        new Cell(valueTypes.boolean, 2);
+      });
+    });
+  });
+  describe('#getProcessedValue()', () => {
+    it('should return 5', () => {
+      const cell = new Cell(valueTypes.number, 5);
+      assert.strictEqual(cell.getProcessedValue(), 5);
+    });
+  });
+  describe('#setValue()', () => {
+    it('should set value to false', () => {
+      const cell = new Cell();
+      cell.setValue(valueTypes.boolean, false);
+      assert.strictEqual(cell.value, false);
+    });
+    it('should throw an exception for false as formula', () => {
+      const cell = new Cell();
+      assert.throws(() => {
+        cell.setValue('formula', false);
+      });
+    });
+  });
+  describe('#setColor()', () => {
+    it('should set color to #123456', () => {
+      const cell = new Cell();
+      cell.setColor('#123456');
+      assert.strictEqual(cell.color, '#123456');
+    });
+    it('should throw an exception for #zzzzzz', () => {
+      const cell = new Cell();
+      assert.throws(() => {
+        cell.setColor('#zzzzzz');
+      });
+    });
+  });
+  describe('#isColorCorrect()', () => {
+    it('should return true for #ab24ef', () => {
+      assert.strictEqual(Cell.isColorCorrect('#ab24ef'), true);
+    });
+    it('should return false for #123', () => {
+      assert.strictEqual(Cell.isColorCorrect('#123'), false);
+    });
+  });
+  describe('#isValueCorrect()', () => {
+    it('should return false for Infinity as number', () => {
+      assert.strictEqual(Cell.isValueCorrect(valueTypes.number, Infinity), false);
+    });
+    it('should return false for NaN as number', () => {
+      assert.strictEqual(Cell.isValueCorrect(valueTypes.number, NaN), false);
+    });
+    it('should return true for "test" as string', () => {
+      assert.strictEqual(Cell.isValueCorrect(valueTypes.string, 'test'), true);
+    });
+  });
+  describe('#getRequiredTypeString()', () => {
+    it('should return "string" for formula type', () => {
+      assert.strictEqual(Cell.getRequiredTypeString(valueTypes.formula), 'string');
+    });
+  });
+  describe('#getDefaultValue()', () => {
+    it('should return "" for formula cell', () => {
+      const value = Cell.getDefaultValue(valueTypes.formula);
+      assert.strictEqual(value, '');
+    });
+    it('should throw an exception for type "wrong"', () => {
+      assert.throws(() => {
+        Cell.getDefaultValue('wrong');
+      });
+    });
+  });
+});
