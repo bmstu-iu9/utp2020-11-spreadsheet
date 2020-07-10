d348ea966a9c8418ce107c805c909a807b88fa44
ZhukDima <zhukdo@gmail.com>
2020-07-11 00:08:12 +0200

    Fixed minor problems in the parser. Tests are done as in # 2.

--- src/lib/parser/app.js
+++ src/lib/parser/app.js
@@ -1,182 +1,181 @@
-'use strict';
+const sum = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const mul = (a, b) => a * b;
+const del = (a, b) => a / b;
+const rem = (a, b) => a % b;
+const exp = (a, b) => ((b == null) ? a : a ** b);
+const unMinus = (a) => -a;
 
-const sum = (a, b) => a + b
-const sub = (a, b) => a - b
-const mul = (a, b) => a * b
-const del = (a, b) => a / b
-const rem = (a, b) => a % b
-const exp = (a, b) => (b == null) ? a : a ** b
-const unMinus = a => -a
+const equal = (a, b) => a === b;
+const more = (a, b) => a > b;
 
-const equal = (a, b) => a == b
-const more = (a, b) => a > b
-
-const makeFunc = (func, args) => [func, args]
-const makeInterval = (a1, a2) => [a1, a2]
-const makeAddress = a => a
+const makeFunc = (func, args) => [func, args];
+const makeInterval = (a1, a2) => [a1, a2];
+const makeAddress = (a) => a;
 
 // все команды написанные выше - условны, являются заглушками
 
-const toInt = a => a.charCodeAt()
+const toInt = (a) => a.charCodeAt();
 
-export class Parser {
-    constructor(inputString) {
-        this.inputString = inputString
-        this.pos = 0
+export default class Parser {
+  constructor(inputString) {
+    this.inputString = inputString;
+    this.pos = 0;
+  }
+
+  static makeParserError(str) { throw new SyntaxError(`Parser syntax error in ${str}!`); }
+
+  next() { this.pos += 1; }
+
+  hasNext() { return this.pos < this.inputString.length; }
+
+  get() { return this.hasNext() ? this.inputString[this.pos] : Parser.makeParserError('get'); }
+
+  checkGet(c) {
+    const res = this.hasNext() && (this.get() === c);
+    if (res) this.next();
+    return res;
+  }
+
+  parseBlock() {
+    if (this.checkGet('=')) return this.parseEquals();
+    this.pos = this.inputString.length;
+    return this.inputString;
+  }
+
+  parseEquals() { return this.parseEqualsHelper(this.parseExpr()); }
+
+  parseEqualsHelper(res) { // 1 == | 2 >= | 3 > | 4 <= | 5 < | 6 !=
+    let op = 0;
+    if (this.checkGet('!')) {
+      if (!this.checkGet('=')) Parser.makeParserError('parseEqualsHelper (only !)');
+      else op = 6;
+    } else if (this.checkGet('=')) {
+      if (!this.checkGet('=')) Parser.makeParserError('parseEqualsHelper (only =)');
+      else op = 1;
+    } else if (this.checkGet('>')) {
+      if (this.checkGet('=')) op = 2;
+      else op = 3;
+    } else if (this.checkGet('<')) {
+      if (this.checkGet('=')) op = 4;
+      else op = 5;
+    } else return res;
+    const res2 = this.parseExpr();
+    switch (op) {
+      case 1: return equal(res, res2);
+      case 2: return !more(res2, res);
+      case 3: return more(res, res2);
+      case 4: return !more(res, res2);
+      case 5: return more(res2, res);
+      default: return !equal(res, res2);
     }
+  }
 
-    makeParserError(str) { throw new SyntaxError(`Parser syntax error in ${str}!`); }
+  parseExpr() { return this.parseExprHelper(this.parseTerm()); }
 
-    next() { this.pos++; }
+  parseExprHelper(res) {
+    if (this.checkGet('-')) return this.parseExprHelper(sub(res, this.parseTerm()));
+    if (this.checkGet('+')) return this.parseExprHelper(sum(res, this.parseTerm()));
+    return res;
+  }
 
-    hasNext() { return this.pos < this.inputString.length; }
+  parseTerm() { return this.parseTermHelper(this.parseFactor()); }
 
-    get() { return this.hasNext() ? this.inputString[this.pos] : this.makeParserError("get"); }
+  parseTermHelper(res) {
+    if (this.checkGet('*')) return this.parseTermHelper(mul(res, this.parseFactor()));
+    if (this.checkGet('/')) return this.parseTermHelper(del(res, this.parseFactor()));
+    if (this.checkGet('%')) return this.parseTermHelper(rem(res, this.parseFactor()));
+    return res;
+  }
 
-    checkGet(c) {
-        let res = this.hasNext() && (this.get() == c)
-        if (res) this.next()
-        return res
+  parseFactor() { return exp(this.parsePower(), this.parseFactorHelper()); }
+
+  parseFactorHelper() {
+    if (this.checkGet('^')) return exp(this.parsePower(), this.parseFactorHelper());
+    return null;
+  }
+
+  parsePower() {
+    if (this.checkGet('(')) {
+      const res = this.parseExpr();
+      if (!this.checkGet(')')) Parser.makeParserError('parsePower (wrong bracket sequence)');
+      return res;
+    } if (this.checkGet('-')) {
+      return unMinus(this.parsePower());
+    } if (this.hasNext() && this.get() >= 'А' && this.get() <= 'Я') {
+      const nameFun = this.parseNameFunc();
+      if (!this.checkGet('(')) Parser.makeParserError('parsePower (no argument)');
+      const args = this.parseArgs();
+      return makeFunc(nameFun, args);
+    } return this.parseValue();
+  }
+
+  parseArgs() {
+    const arr = [];
+    if (this.checkGet(')')) return arr;
+    arr.push(this.parseExpr());
+    return this.parseArgsHelper(arr);
+  }
+
+  parseArgsHelper(arr) {
+    if (this.checkGet(';')) {
+      arr.push(this.parseExpr());
+      return this.parseArgsHelper(arr);
+    } if (this.checkGet(')')) return arr;
+    return Parser.makeParserError('parseArgsHelper');
+  }
+
+  parseValue() {
+    if (this.hasNext() && (this.get() === '"')) {
+      return this.parseStr();
+    } if (this.hasNext() && this.get() >= '0' && this.get() <= '9') {
+      return this.parseNum();
+    } if (this.hasNext() && this.get() >= 'A' && this.get() <= 'Z') {
+      const res = makeAddress(this.parseAddress());
+      if (this.checkGet(':')) {
+        const res2 = makeAddress(this.parseAddress());
+        return makeInterval(res, res2);
+      }
+      return res;
     }
+    return Parser.makeParserError('parseValue');
+  }
 
-    parseBlock() {
-        if (this.checkGet('=')) return this.parseEquals()
-        this.pos = this.inputString.length
-        return this.inputString
+  parseFromTo(from, to, func, start) {
+    if (!(this.hasNext() && from <= this.get() && this.get() <= to)) Parser.makeParserError('parseFromTo');
+    let res = start;
+    while (this.hasNext() && from <= this.get() && this.get() <= to) {
+      res = func(res, this.get());
+      this.next();
     }
+    return res;
+  }
 
-    parseEquals() { return this.parseEqualsHelper(this.parseExpr()); }
+  parseNum() { return this.parseFromTo('0', '9', (res, c) => res * 10 + toInt(c) - toInt('0'), 0); }
 
-    parseEqualsHelper(res) {  // 1 == | 2 >= | 3 > | 4 <= | 5 < | 6 !=
-        let op = 0
-        if (this.checkGet('!')) {
-            if (!this.checkGet('=')) this.makeParserError("parseEqualsHelper (only !)")
-            else op = 6
-        } else if (this.checkGet('=')) {
-            if (!this.checkGet('=')) this.makeParserError("parseEqualsHelper (only =)")
-            else op = 1
-        } else if (this.checkGet('>')) {
-            if (this.checkGet('=')) op = 2
-            else op = 3
-        } else if (this.checkGet('<')) {
-            if (this.checkGet('=')) op = 4
-            else op = 5
-        } else return res
-        let res2 = this.parseExpr()
-        switch (op) {
-            case 1: return equal(res, res2)
-            case 2: return !more(res2, res)
-            case 3: return more(res, res2)
-            case 4: return !more(res, res2)
-            case 5: return more(res2, res)
-            case 6: return !equal(res, res2)
-        }
+  parseNameFunc() { return this.parseFromTo('А', 'Я', (res, c) => res + c, ''); }
+
+  parseAddress() {
+    this.checkGet('$');
+    const ind1 = this.parseFromTo('A', 'Z', (res, c) => res * 26 + toInt(c) - toInt('A'), '');
+    this.checkGet('$');
+    const ind2 = this.parseNum();
+    return [ind1, ind2];
+  }
+
+  parseStr() {
+    if (!this.checkGet('"')) Parser.makeParserError('parseStr');
+    let res = '';
+    while (!this.checkGet('"')) {
+      res += this.get();
+      this.next();
     }
+    return res;
+  }
 
-    parseExpr() { return this.parseExprHelper(this.parseTerm()); }
-
-    parseExprHelper(res) {
-        if (this.checkGet('-')) return this.parseExprHelper(sub(res, this.parseTerm()))
-        else if (this.checkGet('+')) return this.parseExprHelper(sum(res, this.parseTerm()))
-        else return res
-    }
-
-    parseTerm() { return this.parseTermHelper(this.parseFactor()); }
-
-    parseTermHelper(res) {
-        if (this.checkGet('*')) return this.parseTermHelper(mul(res, this.parseFactor()))
-        else if (this.checkGet('/')) return this.parseTermHelper(del(res, this.parseFactor()))
-        else if (this.checkGet('%')) return this.parseTermHelper(rem(res, this.parseFactor()))
-        else return res
-    }
-
-    parseFactor() { return exp(this.parsePower(), this.parseFactorHelper()); }
-
-    parseFactorHelper() {
-        if (this.checkGet('^')) return exp(this.parsePower(), this.parseFactorHelper())
-        else return null
-    }
-
-    parsePower() {
-        if (this.checkGet('(')) {
-            let res = this.parseExpr()
-            if (!this.checkGet(')')) this.makeParserError("parsePower (wrong bracket sequence)")
-            return res
-        } else if (this.checkGet('-')) {
-            return unMinus(this.parsePower())
-        } else if (this.hasNext() && 'А' <= this.get() && this.get() <= 'Я') {
-            let nameFun = this.parseNameFunc()
-            if (!this.checkGet('(')) this.makeParserError("parsePower (no argument)")
-            let args = this.parseArgs()
-            return makeFunc(nameFun, args)
-        } else return this.parseValue()
-    }
-
-    parseArgs() {
-        const arr = []
-        if (this.checkGet(')')) return arr
-        arr.push(this.parseExpr())
-        return this.parseArgsHelper(arr)
-    }
-
-    parseArgsHelper(arr) {
-        if (this.checkGet(';')) {
-            arr.push(this.parseExpr())
-            return this.parseArgsHelper(arr)
-        } else if (this.checkGet(')')) return arr
-        else this.makeParserError("parseArgsHelper")
-    }
-
-    parseValue() {
-        if (this.hasNext() && (this.get() == '\"')) {
-            return this.parseStr()
-        } else if (this.hasNext() && '0' <= this.get() && this.get() <= '9') {
-            return this.parseNum()
-        } else if (this.hasNext() && 'A' <= this.get() && this.get() <= 'Z') {
-            let res = makeAddress(this.parseAddress())
-            if (this.checkGet(':')) {
-                let res2 = makeAddress(this.parseAddress())
-                return makeInterval(res, res2)
-            }
-            return res
-        } else this.makeParserError("parseValue")
-    }
-
-    parseFromTo(from, to, func, start) {
-        if (!(this.hasNext() && from <= this.get() && this.get() <= to)) this.makeParserError("parseFromTo")
-        let res = start
-        while (this.hasNext() && from <= this.get() && this.get() <= to) {
-            res = func(res, this.get())
-            this.next()
-        }
-        return res
-    }
-
-    parseNum() { return this.parseFromTo('0', '9', (res, c) => res * 10 + toInt(c) - toInt('0'), 0); }
-
-    parseNameFunc() { return this.parseFromTo('А', 'Я', (res, c) => res + c, ""); }
-
-    parseAddress() {
-        this.checkGet('$')
-        let ind1 = this.parseFromTo('A', 'Z', (res, c) => res * 26 + toInt(c) - toInt('A'), "")
-        this.checkGet('$')
-        let ind2 = this.parseNum()
-        return [ind1, ind2]
-    }
-
-    parseStr() {
-        if (!this.checkGet('\"')) this.makeParserError("parseStr")
-        let res = ""
-        while (!this.checkGet('\"')) {
-            res += this.get()
-            this.next()
-        }
-        return res
-    }
-
-    run() {
-        let ans = this.parseBlock()
-        if (this.pos < this.inputString.length) this.makeParserError("run")
-        return ans
-    }
+  run() {
+    const ans = this.parseBlock();
+    if (this.pos < this.inputString.length) Parser.makeParserError('run');
+    return ans;
+  }
 }
--- src/tests/lib/parser/test.js
+++ src/tests/lib/parser/test.js
@@ -1,28 +1,154 @@
-import { Parser } from '../../../lib/parser/app.js';
+import * as assert from 'assert';
+import Parser from '../../../lib/parser/app.js';
 
-const test = str => { new Parser(str).run() }
-
-const checkAns = (str, ans) => test(str) == ans
-
-const checkManyAns = strs => {
-    for (let x = 0; x < strs.length; x++) {
-        if (!checkAns(strs[x][0], strs[x][1])) {
-            console.log("Find miss!")
-            console.log(`True ans: ${strs[x][1]} \nFind ans: ${test(strs[x][0])}`)
-            return;
-        }
-    }
-    console.log("All true!")
-}
-
-checkManyAns([
-    [`1`, `1`],
-    [`"asdf"`, `"asdf"`],
-    [`12bc`, `12bc`],
-    [`="abc"+"def"`, `abcdef`],
-    [`=(1+5^(1/2))/2`, (1 + Math.sqrt(5)) / 2],
-    [`=(1-5^(1/2))/2`, (1 - Math.sqrt(5)) / 2],
-    [`=СУММА(A2:F2;B3:ZA3)`, [`СУММА`, [[[0, 2], [5, 2]], [[2, 3], [550, 3]]]]],
-    [`=A1`, [0, 1]],
-    [`=1+"b"+3+"c"`, `1b3c`],
-])
+describe('Parser', () => {
+  describe('#run()', () => {
+    it('should parse liquid values', () => {
+      assert.strictEqual(new Parser('123').run(), '123');
+      assert.strictEqual(new Parser('abd123efg').run(), 'abd123efg');
+      assert.strictEqual(new Parser('=1+2+3').run(), 1 + 2 + 3);
+      assert.strictEqual(new Parser('=A1').run(), new Parser('A1').parseAddress());
+    });
+    it('should parse illiquid values', () => {
+      assert.throws(() => { new Parser('=A2B').run(); });
+      assert.throws(() => { new Parser('=').run(); });
+      assert.throws(() => { new Parser('=1+((5*8)').run(); });
+    });
+  });
+  describe('#parseBlock()', () => {
+    it('should parse not function', () => {
+      assert.strictEqual(new Parser('123').run(), '123');
+      assert.strictEqual(new Parser('abd123efg').run(), 'abd123efg');
+    });
+    it('should parse function', () => {
+      assert.strictEqual(new Parser('=1+2+3').run(), 1 + 2 + 3);
+      assert.strictEqual(new Parser('=(1+5^(1/2))/2').run(), (1 + Math.sqrt(5)) / 2);
+      assert.strictEqual(new Parser('=5*(2+2)').run(), 5 * (2 + 2));
+      assert.strictEqual(new Parser('=A1').run(), [0, 1]);
+    });
+  });
+  describe('#parseEquals()', () => {
+    it('should parse liquid logic functions (1)', () => {
+      assert.strictEqual(new Parser('=5==2+2').run(), false);
+      assert.strictEqual(new Parser('=5>=2+2').run(), true);
+      assert.strictEqual(new Parser('=5>2+2').run(), true);
+      assert.strictEqual(new Parser('=5<=2+2').run(), false);
+      assert.strictEqual(new Parser('=5<2+2').run(), false);
+      assert.strictEqual(new Parser('=5!=2+2').run(), true);
+    });
+    it('should parse liquid logic functions (2)', () => {
+      assert.strictEqual(new Parser('=3==2+2').run(), false);
+      assert.strictEqual(new Parser('=3>=2+2').run(), false);
+      assert.strictEqual(new Parser('=3>2+2').run(), false);
+      assert.strictEqual(new Parser('=3<=2+2').run(), true);
+      assert.strictEqual(new Parser('=3<2+2').run(), true);
+      assert.strictEqual(new Parser('=3!=2+2').run(), true);
+    });
+    it('should parse liquid logic functions (3)', () => {
+      assert.strictEqual(new Parser('=4==2+2').run(), true);
+      assert.strictEqual(new Parser('=4>=2+2').run(), true);
+      assert.strictEqual(new Parser('=4>2+2').run(), false);
+      assert.strictEqual(new Parser('=4<=2+2').run(), true);
+      assert.strictEqual(new Parser('=4<2+2').run(), false);
+      assert.strictEqual(new Parser('=4!=2+2').run(), false);
+    });
+    it('should parse illiquid logic functions', () => {
+      assert.throws(() => { new Parser('=2<3<4').run(); });
+      assert.throws(() => { new Parser('=2<').run(); });
+      assert.throws(() => { new Parser('=<3').run(); });
+      assert.throws(() => { new Parser('=2!3').run(); });
+      assert.throws(() => { new Parser('=2=3').run(); });
+    });
+  });
+  describe('#parseExpr()', () => {
+    it('should parse liquid summarizing functions', () => {
+      assert.strictEqual(new Parser('=2+2').run(), 4);
+      assert.strictEqual(new Parser('=13+27').run(), 40);
+      assert.strictEqual(new Parser('=436542641+23152654').run(), 436542641 + 23152654);
+    });
+    it('should parse liquid subtractive functions', () => {
+      assert.strictEqual(new Parser('=2-2').run(), 0);
+      assert.strictEqual(new Parser('=13-27').run(), -14);
+      assert.strictEqual(new Parser('=436542641-23152654').run(), 436542641 - 23152654);
+    });
+  });
+  describe('#parseTerm()', () => {
+    it('should parse liquid multiplying functions', () => {
+      assert.strictEqual(new Parser('=2*2').run(), 4);
+      assert.strictEqual(new Parser('=13*27').run(), 13 * 27);
+      assert.strictEqual(new Parser('=436542*23152').run(), 436542 * 23152);
+    });
+    it('should parse liquid dividing functions', () => {
+      assert.strictEqual(new Parser('=2/2').run(), 2 / 2);
+      assert.strictEqual(new Parser('=13/27').run(), 13 / 27);
+      assert.strictEqual(new Parser('=436542641/23152654').run(), 436542641 / 23152654);
+    });
+    it('should parse liquid residual  functions', () => {
+      assert.strictEqual(new Parser('=2%2').run(), 2 % 2);
+      assert.strictEqual(new Parser('=13%27').run(), 13 % 27);
+      assert.strictEqual(new Parser('=436542641%23152654').run(), 436542641 % 23152654);
+    });
+  });
+  describe('#parseFactor()', () => {
+    it('should parse liquid power functions', () => {
+      assert.strictEqual(new Parser('=2^2').run(), 4);
+      assert.strictEqual(new Parser('=1^1000').run(), 1);
+      assert.strictEqual(new Parser('=0^1000').run(), 0);
+      assert.strictEqual(new Parser('=2^10').run(), 1024);
+    });
+  });
+  describe('#parsePower() && #parseArgs()', () => {
+    it('should parse liquid bracket sequence', () => {
+      assert.strictEqual(new Parser('=(((1+1)+1)+1)+1').run(), 5);
+      assert.strictEqual(new Parser('=((((2))+(((2)))))').run(), 4);
+    });
+    it('should parse illiquid bracket sequence', () => {
+      assert.throws(() => { new Parser('=((1+1)+1)+1)+1').run(); });
+      assert.throws(() => { new Parser('=((((2))+(((2))))').run(); });
+    });
+    it('should parse liquid functions with unary minus', () => {
+      assert.strictEqual(new Parser('=-1').run(), -1);
+      assert.strictEqual(new Parser('=(-2)^3').run(), -8);
+      assert.strictEqual(new Parser('=5*(-5)').run(), -25);
+    });
+    it('should parse liquidity function name', () => {
+      assert.strictEqual(new Parser('=СУММА(1;2)').run(), ['СУММА', [1, 2]]);
+      assert.strictEqual(new Parser('=КОРЕНЬ(5)').run(), ['КОРЕНЬ', [5]]);
+      assert.strictEqual(new Parser('=ЛЮБЛЮПИСАТЬТЕСТЫ()').run(), ['ЛЮБЛЮПИСАТЬТЕСТЫ', []]);
+      assert.strictEqual(new Parser('=ИУТОП()').run(), ['ИУТОП', []]);
+    });
+    it('should parse illiquidity function name', () => {
+      assert.throws(() => { new Parser('=СУММА').run(); });
+      assert.throws(() => { new Parser('=КОРЕНЬ(5').run(); });
+      assert.throws(() => { new Parser('=КОРЕНЬ(5;0').run(); });
+      assert.throws(() => { new Parser('=ЛЮБЛЮПИСАТЬТЕСТЫ').run(); });
+      assert.throws(() => { new Parser('=ИУТОП').run(); });
+    });
+  });
+  describe('#parseStr()', () => {
+    it('should parse liquid strings', () => {
+      assert.strictEqual(new Parser('="2^2"').run(), '2^2');
+      assert.strictEqual(new Parser('="a"+"b"').run(), 'ab');
+      assert.strictEqual(new Parser('="A"+1+":B"+2').run(), 'A1:B2');
+    });
+    it('should parse illiquid strings', () => {
+      assert.throws(() => { new Parser('="a').run(); });
+      assert.throws(() => { new Parser('="a""b"').run(); });
+      assert.throws(() => { new Parser('=b"').run(); });
+    });
+  });
+  describe('#parseValue()', () => {
+    it('should parse illiquid something', () => {
+      assert.throws(() => { new Parser('=*').run(); });
+      assert.throws(() => { new Parser('=~').run(); });
+      assert.throws(() => { new Parser('=^').run(); });
+    });
+    it('should parse liquid interval', () => {
+      assert.strictEqual(new Parser('=A2:B8').run(), [[0, 2], [1, 8]]);
+    });
+    it('should parse illiquid interval', () => {
+      assert.throws(() => { new Parser('=A2:B').run(); });
+      assert.throws(() => { new Parser('=A2:').run(); });
+    });
+  });
+});
