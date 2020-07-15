f5bb9fc10ba79c3dcc9ce68b936eeb8a76b56ecf
AlexisOMG <alesha-gramazeka@yandex.ru>
2020-07-15 20:30:49 +0300

    Added JsonConverter, tests, done some refactoring

--- src/lib/readWorkbook/JsonConverter.js
+++ src/lib/readWorkbook/JsonConverter.js
@@ -1,22 +1,16 @@
 import fs from 'fs';
-import schema from 'jsonschema';
 import { Cell } from '../spreadsheets/Cell.js';
 import Spreadsheet from '../spreadsheets/Spreadsheet.js';
 import Workbook from '../spreadsheets/Workbook.js';
 
-const workbookSchema = JSON.parse(fs.readFileSync('./resources/tableSchema.json'));
-
 export default class JsonConverter {
-  static readCells(cells) {
-    const resultCells = new Map();
-    const keys = Object.keys(cells);
-    keys.forEach((key) => {
-      resultCells.set(key, new Cell(cells[key].type, cells[key].value, cells[key].color));
-    });
-    return resultCells;
+  static readWorkbook(pathToWorkbook) {
+    const file = JSON.parse(fs.readFileSync(pathToWorkbook));
+    const workbook = new Workbook(file.name, this.readSpreadsheets(file.sheets));
+    return workbook;
   }
 
-  static readSpreadsheets(spreadsheets) {
+  static readSpreadsheets(spreadsheets = []) {
     const resultSpreadsheets = [];
     spreadsheets.forEach((spreadsheet) => {
       resultSpreadsheets.push(new Spreadsheet(spreadsheet.name, this.readCells(spreadsheet.cells)));
@@ -24,12 +18,12 @@ export default class JsonConverter {
     return resultSpreadsheets;
   }
 
-  static readWorkbook(pathToWorkbook) {
-    const file = JSON.parse(fs.readFileSync(pathToWorkbook));
-    if (schema.validate(file, workbookSchema).valid === false) {
-      throw new Error('Invalid workbook');
-    }
-    const workbook = new Workbook(file.name, this.readSpreadsheets(file.sheets));
-    return workbook;
+  static readCells(cells = {}) {
+    const resultCells = new Map();
+    const keys = Object.keys(cells);
+    keys.forEach((key) => {
+      resultCells.set(key, new Cell(cells[key].type, cells[key].value, cells[key].color));
+    });
+    return resultCells;
   }
 }
--- src/tests/lib/readWorkbook/JsonConverter.test.js
+++ src/tests/lib/readWorkbook/JsonConverter.test.js
@@ -1,7 +1,6 @@
 import * as assert from 'assert';
-import fs from 'fs';
 import mock from 'mock-fs';
-import schema from 'jsonschema';
+import fs from 'fs';
 import Workbook from '../../../lib/spreadsheets/Workbook.js';
 import Spreadsheet from '../../../lib/spreadsheets/Spreadsheet.js';
 import ClassConverter from '../../../lib/saveWorkbook/ClassConverter.js';
@@ -13,16 +12,97 @@ import {
 
 const workbookStandardName = 'workbook';
 const spreadsheetStandardName = 'spreadsheet';
-const pathToWorkbooks = '/home/alexis/project/utp2020-11-spreadsheet/src/workbooks/alexis';
+const pathToWorkbooks = './workbooks/alexis';
 
-const cells = new Map();
-cells.set('A1', new Cell(valueTypes.number, 10));
-const spreadsheets = [new Spreadsheet(spreadsheetStandardName, cells)];
-const workbook = new Workbook(workbookStandardName, spreadsheets);
-console.log(workbook);
-//ClassConverter.saveJson(workbook, pathToWorkbooks);
-const test = JsonConverter.readWorkbook(`${pathToWorkbooks}/${workbookStandardName}.json`);
-console.log(test);
-if (JSON.stringify(test) === JSON.stringify(workbook)) {
-  console.log('FUUUCK YEAAAH');
-}
+describe('JsonConverter', () => {
+  describe('#readCells()', () => {
+    it('should return a map of cells', () => {
+      let cells = {
+        A1: {
+          color: '#ffffff',
+          type: valueTypes.number,
+          value: 10,
+        },
+        A2: {
+          color: '#ffffff',
+          type: valueTypes.number,
+          value: 11,
+        },
+        A3: {
+          color: '#ffffff',
+          type: valueTypes.number,
+          value: 13,
+        },
+      };
+      cells = JsonConverter.readCells(cells);
+      const testCells = new Map();
+      testCells.set('A1', new Cell(valueTypes.number, 10));
+      testCells.set('A2', new Cell(valueTypes.number, 11));
+      testCells.set('A3', new Cell(valueTypes.number, 13));
+      assert.strictEqual(JSON.stringify(cells), JSON.stringify(testCells));
+    });
+    it('should return an empty map', () => {
+      assert.strictEqual(JSON.stringify(JsonConverter.readCells({})), JSON.stringify(new Map()));
+    });
+  });
+  describe('#readSpreadsheets()', () => {
+    it('should return an array of sheets', () => {
+      let spreadsheets = [
+        {
+          name: 'Sheet1',
+          cells: {
+            A1: {
+              color: '#ffffff',
+              type: valueTypes.number,
+              value: 10,
+            },
+          },
+        },
+        {
+          name: 'Sheet2',
+          cells: {
+            A2: {
+              color: '#ffffff',
+              type: valueTypes.boolean,
+              value: true,
+            },
+          },
+        },
+      ];
+      spreadsheets = JsonConverter.readSpreadsheets(spreadsheets);
+      const testSpreadsheets = [
+        new Spreadsheet('Sheet1', new Map([['A1', new Cell(valueTypes.number, 10)]])),
+        new Spreadsheet('Sheet2', new Map([['A2', new Cell(valueTypes.boolean, true)]])),
+      ];
+      assert.strictEqual(JSON.stringify(spreadsheets), JSON.stringify(testSpreadsheets));
+    });
+    it('should return an empty array', () => {
+      assert.strictEqual(JSON.stringify(JsonConverter.readSpreadsheets([])), JSON.stringify([]));
+    });
+  });
+  describe('#readWorkbook()', () => {
+    it('should read Workbook from correct json-file', () => {
+      mock({
+        './workbooks': {},
+      });
+      const cells = new Map();
+      cells.set('A1', new Cell(valueTypes.number, 10));
+      const spreadsheets = [new Spreadsheet(spreadsheetStandardName, cells)];
+      const workbook = new Workbook(workbookStandardName, spreadsheets);
+      ClassConverter.saveJson(workbook, pathToWorkbooks);
+      const testWorkbook = JsonConverter.readWorkbook(`${pathToWorkbooks}/${workbookStandardName}.json`);
+      assert.strictEqual(JSON.stringify(workbook), JSON.stringify(testWorkbook));
+      mock.restore();
+    });
+    it('should throw an exception for a nonexistent file', () => {
+      mock({
+        './workbooks': {},
+      });
+      fs.mkdirSync(pathToWorkbooks, { recursive: true });
+      assert.throws(() => {
+        JsonConverter.readWorkbook(`${pathToWorkbooks}/${workbookStandardName}.json`);
+      });
+      mock.restore();
+    });
+  });
+});
