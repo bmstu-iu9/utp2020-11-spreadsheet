8cbd59705ce75ad87caaa20a09b4eeccfd6ac552
Артём Белоусов <flygrounder@yandex.ru>
2020-07-15 13:28:36 +0300

    Added NumberComparisonFilter

---
+++ src/tests/lib/filters/NumberComparisonFilter.test.js
@@ -0,0 +1,65 @@
+import * as assert from 'assert';
+import { operators, NumberComparisonFilter } from '../../../lib/filters/NumberComparisonFilter.js';
+import { Cell, valueTypes } from '../../../lib/spreadsheets/Cell.js';
+
+describe('NumberComparisonFilter', () => {
+  describe('#constructor()', () => {
+    it('should not throw an exception for "A", "more", 5', () => {
+      assert.doesNotThrow(() => {
+        new NumberComparisonFilter('A', operators.more, 5);
+      });
+    });
+    it('should not throw an exception for "A", "moreOrLess", 5', () => {
+      assert.throws(() => {
+        new NumberComparisonFilter('A', 'moreOrLess', 5);
+      });
+    });
+  });
+  describe('#doesCellMatch()', () => {
+    const testCases = [{
+      description: 'should return true for 5 = 5',
+      filter: ['A', operators.equal, 5],
+      cell: [valueTypes.number, 5],
+      result: true,
+    },
+    {
+      description: 'should return false for 5 > 6',
+      filter: ['A', operators.more, 6],
+      cell: [valueTypes.number, 5],
+      result: false,
+    },
+    {
+      description: 'should return true for 6 >= 6',
+      filter: ['A', operators.moreOrEqual, 6],
+      cell: [valueTypes.number, 6],
+      result: true,
+    },
+    {
+      description: 'should return false for 7 < -1',
+      filter: ['A', operators.less, -1],
+      cell: [valueTypes.number, 7],
+      result: false,
+    },
+    {
+      description: 'should return true for -1 <= 0',
+      filter: ['A', operators.lessOrEqual, 0],
+      cell: [valueTypes.number, -1],
+      result: true,
+    },
+    {
+      description: 'should return false for -10 != -10',
+      filter: ['A', operators.notEqual, -10],
+      cell: [valueTypes.number, -10],
+      result: false,
+    },
+    ];
+    testCases.forEach((testCase) => {
+      it(testCase.description, () => {
+        const numberComparisonFilter = new NumberComparisonFilter(...testCase.filter);
+        const cell = new Cell(...testCase.cell);
+        const result = numberComparisonFilter.doesCellMatch(cell);
+        assert.equal(result, testCase.result);
+      });
+    });
+  });
+});
