66d3a190b0c26abac91a968bd7029fecb842872f
ZhukDima <zhukdo@gmail.com>
2020-07-17 23:52:20 +0300

    Переписанна часть функций класса синхронизации. Обновлены тесты

--- src/server/synchronization/synchronizer.js
+++ src/server/synchronization/synchronizer.js
@@ -1,127 +1,52 @@
 import fs from 'fs';
 
-const copyJson = (json) => JSON.parse(JSON.stringify(json));
-
 export default class Synchronizer {
   constructor(pathToWorkbook, page) {
+    this.pathToWorkbook = pathToWorkbook;
     this.jsonWorkbook = JSON.parse(fs.readFileSync(pathToWorkbook));
     this.page = page;
-    this.logs = [];
-    this.cancelChange = 0;
+    this.lastChanges = new Map();
     this.IDlogs = 0;
-
-    this.lastJsonWorkbook = copyJson(this.jsonWorkbook);
-    this.lastPosLogs = 0;
-  }
-
-  static maxLogSize() {
-    return 20;
-  } // длина максимального возможного отката - Ctrl+Z
-
-  static minLogSize() {
-    return 10;
-  } // длина максимального возможного лога
-
-  addLog(log, check = true) {
-    this.IDlogs += 1;
-    switch (log.type) {
-      case 'cancelChange': {
-        if (this.cancelChange === Synchronizer.minLogSize()
-        || this.cancelChange === this.logs.length) {
-          throw new TypeError('roll back log change more change log');
-        }
-        this.cancelChange += 1;
-        break;
-      }
-      case 'cancelСancelChange': {
-        if (this.cancelChange === 0) {
-          throw new TypeError('haven\'t cansel roll back log');
-        }
-        this.cancelChange -= 1;
-        break;
-      }
-      default: {
-        if (this.cancelChange !== 0) {
-          if (this.logs.length - this.cancelChange < this.lastPosLogs) {
-            this.lastJsonWorkbook = copyJson(this.jsonWorkbook);
-            this.lastPosLogs = 0;
-          }
-          this.logs = this.logs.slice(0, this.logs.length - this.cancelChange);
-          this.cancelChange = 0;
-        }
-        this.logs.push(log);
-        if (check) {
-          this.updateLogSize();
-        }
-      }
-    }
   }
 
   addArrayLogs(arrayLogs) {
-    arrayLogs.forEach((log) => this.addLog(log, false));
-    this.updateLogSize();
-  }
-
-  updateLogSize() {
-    if (this.logs.length > Synchronizer.maxLogSize()) {
-      if (this.logs.length - Synchronizer.minLogSize() === this.lastPosLogs) {
-        this.jsonWorkbook = copyJson(this.lastJsonWorkbook);
-        this.lastPosLogs = 0;
-      } else if (this.logs.length - Synchronizer.minLogSize() < this.lastPosLogs) {
-        this.updateWorkbook(
-          this.jsonWorkbook, 0, this.logs.length - Synchronizer.minLogSize(),
-        );
-        this.lastPosLogs = this.lastPosLogs + Synchronizer.minLogSize() - this.logs.length;
-      } else {
-        this.updateWorkbook(
-          this.lastJsonWorkbook, this.lastPosLogs, this.logs.length - Synchronizer.minLogSize(),
-        );
-        this.jsonWorkbook = copyJson(this.lastJsonWorkbook);
-        this.lastPosLogs = 0;
-      }
-      this.logs = this.logs.slice(this.logs.length - Synchronizer.minLogSize(), this.logs.length);
-    }
-  }
-
-  updateWorkbook(jsonWorkbook = this.lastJsonWorkbook,
-    from = this.lastPosLogs, to = this.logs.length - this.cancelChange) {
-    this.logs
-      .slice(from, to)
-      .forEach((log) => {
-        switch (log.type) {
-          case 'color': {
-            const { cells } = jsonWorkbook.sheets[this.page];
-            if (cells[log.cellAddress] === undefined) {
-              cells[log.cellAddress] = {};
-            }
-            cells[log.cellAddress].color = log.color;
-            break;
+    this.IDlogs += 1;
+    arrayLogs.forEach((log) => {
+      switch (log.type) {
+        case 'color': {
+          const { cells } = this.jsonWorkbook.sheets[this.page];
+          if (cells[log.cellAddress] === undefined) {
+            cells[log.cellAddress] = {};
           }
-          case 'value': {
-            const { cells } = jsonWorkbook.sheets[this.page];
-            if (cells[log.cellAddress] === undefined) {
-              cells[log.cellAddress] = {};
-            }
-            cells[log.cellAddress].type = log.valueType;
-            cells[log.cellAddress].value = log.value;
-            break;
-          }
-          default:
-            throw new TypeError('undefined log change type');
+          cells[log.cellAddress].color = log.color;
+          break;
         }
-      });
-    return jsonWorkbook;
+        case 'value': {
+          const { cells } = this.jsonWorkbook.sheets[this.page];
+          if (cells[log.cellAddress] === undefined) {
+            cells[log.cellAddress] = {};
+          }
+          cells[log.cellAddress].type = log.valueType;
+          cells[log.cellAddress].value = log.value;
+          break;
+        }
+        default:
+          throw new TypeError('undefined log change type');
+      }
+    });
+    const last = arrayLogs[arrayLogs.length - 1];
+    const inMap = this.lastChanges.get(`${last.cellAddress}/${last.type}`);
+    const ans = (inMap !== undefined) ? { first: inMap, second: last } : undefined;
+    // при коллизия изменений - изменение также применяется, но начинает возвращать пару
+    this.lastChanges.set(`${last.cellAddress}/${last.type}`, last);
+    return ans;
+  }
+
+  clearCheckChanges() {
+    this.lastChanges = new Map();
   }
 
   synchronize() {
-    if (this.logs.length - this.cancelChange !== this.lastPosLogs) {
-      if (this.logs.length - this.cancelChange < this.lastPosLogs) {
-        this.lastJsonWorkbook = copyJson(this.jsonWorkbook);
-        this.lastPosLogs = 0;
-      }
-      this.updateWorkbook();
-      this.lastPosLogs = this.logs.length - this.cancelChange;
-    }
-    return { ID: this.IDlogs, jsonWorkbook: this.lastJsonWorkbook };
+    fs.writeFileSync(this.pathToWorkbook, JSON.stringify(this.jsonWorkbook));
   }
 }
--- src/tests/server/synchronizer.test.js
+++ src/tests/server/synchronizer.test.js
@@ -9,30 +9,15 @@ const workbook = {
     {
       name: 'My Sheet',
       cells: {
-        A1: {
+        A5: {
           color: '#ffffff',
           type: 'number',
           value: 100,
         },
-        A2: {
+        A6: {
           color: '#edeef0',
           type: 'boolean',
           value: true,
-        },
-      },
-    },
-    {
-      name: 'My Sheet',
-      cells: {
-        A1: {
-          color: '#000000',
-          type: 'formula',
-          value: '=B2+1',
-        },
-        B2: {
-          color: '#aaaaaa',
-          type: 'string',
-          value: 'some string',
         },
       },
     },
@@ -45,32 +30,23 @@ const log1 = {
 };
 const log2 = {
   type: 'value',
-  cellAddress: 'B1',
+  cellAddress: 'A2',
   valueType: 'formula',
   value: '=2+2',
 };
 const log3 = {
-  type: 'value',
-  cellAddress: 'A1',
-  valueType: 'number',
-  value: '99',
+  type: 'pupailupa',
 };
-const cancelLog = {
-  type: 'cancelChange',
-};
-const log5 = {
-  type: 'color',
-  cellAddress: 'B2',
-  value: '#edeef0',
-};
-const cancelCancelLog = {
-  type: 'cancelСancelChange',
-};
-
 describe('Synchronizer', () => {
   mock({
     './synchronizer': {},
   });
+  const log4 = {
+    type: 'value',
+    cellAddress: 'A2',
+    valueType: 'formula',
+    value: '=4',
+  };
   fs.mkdirSync('./synchronizer/way', { recursive: true });
   fs.writeFileSync('./synchronizer/way/test.json', JSON.stringify(workbook));
   describe('#constructor()', () => {
@@ -80,102 +56,45 @@ describe('Synchronizer', () => {
       assert.deepEqual(sz.page, 0);
     });
   });
-  describe('#addLog()', () => {
-    const sz = new Synchronizer('./synchronizer/way/test.json', 0);
-    it('should add valid log without cancel change', () => {
-      sz.addLog(log1);
-      sz.addLog(log2);
-      sz.addLog(log3);
-      assert.deepEqual(sz.logs, [log1, log2, log3]);
-    });
-    it('should add valid log with cancel change', () => {
-      sz.addLog(cancelLog);
-      sz.addLog(log5);
-      assert.deepEqual(sz.logs, [log1, log2, log5]);
-      sz.synchronize();
-      sz.addLog(cancelLog);
-      sz.addLog(log5);
-      assert.deepEqual(sz.logs, [log1, log2, log5]);
-    });
-    it('should add valid log with cancel-cancel change', () => {
-      sz.addLog(cancelLog);
-      assert.deepEqual(sz.cancelChange, 1);
-      sz.addLog(cancelCancelLog);
-      assert.deepEqual(sz.logs, [log1, log2, log5]);
-      assert.deepEqual(sz.cancelChange, 0);
-    });
-    it('should add invalid log with cancel change', () => {
-      sz.addLog(cancelLog);
-      sz.addLog(cancelLog);
-      sz.addLog(cancelLog);
-      assert.throws(() => {
-        sz.addLog(cancelLog);
-      });
-    });
-    it('should add invalid log with cancel-cancel change', () => {
-      sz.addLog(cancelCancelLog);
-      sz.addLog(cancelCancelLog);
-      sz.addLog(cancelCancelLog);
-      assert.throws(() => {
-        sz.addLog(cancelCancelLog);
-      });
-    });
-  });
   describe('#addArrayLogs()', () => {
     const sz = new Synchronizer('./synchronizer/way/test.json', 0);
-    it('should add array logs', () => {
-      const ar = [log3, log5, log1];
-      sz.addArrayLogs(ar);
-      assert.deepEqual(sz.logs, ar);
+    it('should add valid log (change color)', () => {
+      assert.deepEqual(sz.addArrayLogs([log1]), undefined);
+      assert.deepEqual(sz.jsonWorkbook.sheets[sz.page]
+        .cells[log1.cellAddress].color, log1.color);
     });
-  });
-  describe('#updateLogSize()', () => {
-    const sz = new Synchronizer('./synchronizer/way/test.json', 0);
-    const ar = [];
-    for (let i = 0; i < Synchronizer.maxLogSize() + 1; i += 1) {
-      ar.push(log1);
-    }
-    it('should use after update: static workbook', () => {
-      sz.logs = [];
-      sz.lastPosLogs = ar.length - Synchronizer.minLogSize() - 1;
-      sz.addArrayLogs(ar);
-      assert.deepEqual(sz.logs.length, Synchronizer.minLogSize());
+    it('should add valid log (change value)', () => {
+      assert.deepEqual(sz.addArrayLogs([log2]), undefined);
+      assert.deepEqual(sz.jsonWorkbook.sheets[sz.page]
+        .cells[log2.cellAddress].type, log2.valueType);
+      assert.deepEqual(sz.jsonWorkbook.sheets[sz.page]
+        .cells[log2.cellAddress].value, log2.value);
     });
-    it('should use after update: last workbook', () => {
-      sz.logs = [];
-      sz.lastPosLogs = ar.length - Synchronizer.minLogSize();
-      sz.addArrayLogs(ar);
-      assert.deepEqual(sz.logs.length, Synchronizer.minLogSize());
-    });
-    it('should use after update: new workbook (from last)', () => {
-      sz.logs = [];
-      sz.lastPosLogs = ar.length - Synchronizer.minLogSize() + 1;
-      sz.addArrayLogs(ar);
-      assert.deepEqual(sz.logs.length, Synchronizer.minLogSize());
-    });
-  });
-  describe('#updateWorkbook()', () => {
-    const sz = new Synchronizer('./synchronizer/way/test.json', 0);
-    it('should update workbook with invalid type log', () => {
-      sz.addLog({ type: 'invalid', value: 'me' });
+    it('should add invalid log', () => {
       assert.throws(() => {
-        sz.updateWorkbook();
+        sz.addArrayLogs([log3]);
       });
+    });
+    it('should add collision log', () => {
+      assert.deepEqual(sz.addArrayLogs([log4]), { first: log2, second: log4 });
+      assert.deepEqual(sz.jsonWorkbook.sheets[sz.page]
+        .cells[log4.cellAddress].type, log4.valueType);
+      assert.deepEqual(sz.jsonWorkbook.sheets[sz.page]
+        .cells[log4.cellAddress].value, log4.value);
+    });
+  });
+  describe('#clearCheckChanges()', () => {
+    const sz = new Synchronizer('./synchronizer/way/test.json', 0);
+    it('should clear check changes', () => {
+      sz.addArrayLogs([log1, log2]);
+      sz.clearCheckChanges();
+      assert.deepEqual(sz.lastChanges, new Map());
     });
   });
   describe('#synchronize()', () => {
     const sz = new Synchronizer('./synchronizer/way/test.json', 0);
-    it('should return: static workbook', () => {
-      sz.synchronize();
-    });
-    it('should return: last workbook', () => {
-      sz.addLog(log1);
-      sz.synchronize();
-    });
-    it('should return: new workbook (from static)', () => {
-      sz.addLog(cancelLog);
-      sz.synchronize();
-    });
+    sz.synchronize();
+    it('should synchronize', () => {});
   });
   mock.restore();
 });
