ea553a40497d0a0cdc14d6a2e2054eb1cae94940
ZhukDima <zhukdo@gmail.com>
2020-07-19 19:06:47 +0300

    Лечим ошибку тестирования №2

--- src/server/main.js
+++ src/server/main.js
@@ -1,42 +1,42 @@
-// const express = require('express');
-// const http = require('http');
-// const path = require('path');
+const express = require('express');
+const http = require('http');
+const path = require('path');
 
-// const config = { port: 3000 };
+const config = { port: 3000 };
 
-// const app = express();
+const app = express();
 
-// app.set('view options', { layout: false });
-// app.use(express.static(`${__dirname}/template`));
+app.set('view options', { layout: false });
+app.use(express.static(`${__dirname}/template`));
 
-// app.use(express.favicon());
+app.use(express.favicon());
 
-// if (app.get('env') === 'development') {
-//   app.use(express.logger('dev'));
-// } else {
-//   app.use(express.logger('default'));
-// }
+if (app.get('env') === 'development') {
+  app.use(express.logger('dev'));
+} else {
+  app.use(express.logger('default'));
+}
 
-// app.use(express.bodyParser());
+app.use(express.bodyParser());
 
-// app.use(express.cookieParser());
+app.use(express.cookieParser());
 
-// app.use(app.router);
+app.use(app.router);
 
-// app.get('/', (req, res) => {
-//   res.render('/index.html');
-// });
+app.get('/', (req, res) => {
+  res.render('/index.html');
+});
 
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((err, req, res, next) => {
-//   // NODE_ENV = 'production'
-//   if (app.get('env') === 'development') {
-//     const errorHandler = express.errorHandler();
-//     errorHandler(err, req, res, next);
-//   } else {
-//     res.send(500);
-//   }
-// });
+app.use((err, req, res, next) => {
+  // NODE_ENV = 'production'
+  if (app.get('env') === 'development') {
+    const errorHandler = express.errorHandler();
+    errorHandler(err, req, res, next);
+  } else {
+    res.send(500);
+  }
+});
 
-// http.createServer(app).listen(config.port, () => {});
+http.createServer(app).listen(config.port, () => {});
