2d2eefc6fc1c100b237e3c8d52f0486d94156f8b
AlexisOMG <alesha-gramazeka@yandex.ru>
2020-07-19 13:57:06 +0300

    Fixed codestyle

--- src/lib/validation/Validation.js
+++ src/lib/validation/Validation.js
@@ -5,47 +5,50 @@ import UserModel from '../../server/database/UserModel.js';
 export default class Validation {
   static validate(login, password, isRegistration, pathToDatabase) {
     const database = new Database(pathToDatabase);
+    let result = '';
     if (isRegistration === true) {
-      const result = this.validateRegistration(login, password, database);
-      database.close();
-      return result;
+      result = this.validateRegistration(login, password, database);
     } else {
-      const result = this.validateAuthorization(login, password, database);
-      database.close();
-      return result;
+      result = this.validateAuthorization(login, password, database);
     }
+    database.close();
+    return result;
   }
 
   static validateRegistration(login, password, database) {
     const repo = new UserRepo(database);
     const checkUser = repo.get(login);
+    let result = '';
     if (login.length === 0) {
-      return 'Short login';
-    } else if (emptyString(login)) {
-      return 'Empty login';
+      result = 'Short login';
+    } else if (this.emptyString(login)) {
+      result = 'Empty login';
     } else if (typeof checkUser !== 'undefined') {
-      return 'Need to change login';
+      result = 'Need to change login';
     } else if (password.length === 0) {
-      return 'Short password';
-    } else if (emptyString(password)) {
-      return 'Empty password';
+      result = 'Short password';
+    } else if (this.emptyString(password)) {
+      result = 'Empty password';
     } else {
-      return 'OK';
+      result = 'OK';
     }
+    return result;
   }
 
   static validateAuthorization(login, password, database) {
     const repo = new UserRepo(database);
     const checkUser = repo.get(login);
+    let result = '';
     if (typeof checkUser === 'undefined') {
-      return 'Nonexistent user';
-    } else if (!checkLogin(login, checkUser.login)) {
-      return 'Incorrect login';
-    } else if (!checkPassword(UserModel.getHashedPassword(password), checkUser.password)) {
-      return 'Incorrect password';
+      result = 'Nonexistent user';
+    } else if (!this.checkLogin(login, checkUser.login)) {
+      result = 'Incorrect login';
+    } else if (!this.checkPassword(UserModel.getHashedPassword(password), checkUser.password)) {
+      result = 'Incorrect password';
     } else {
-      return 'OK';
+      result = 'OK';
     }
+    return result;
   }
 
   static emptyString(str) {
