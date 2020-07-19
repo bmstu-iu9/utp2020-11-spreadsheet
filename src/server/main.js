5a820d81fee450a2c35e70a6bf8ce6dadab9e1ab
ZhukDima <zhukdo@gmail.com>
2020-07-19 18:37:16 +0300

    ...

--- src/server/main.js
+++ src/server/main.js
@@ -1,25 +1,25 @@
-import express, { static, favicon, logger, bodyParser, cookieParser, errorHandler as _errorHandler } from 'express';
-import { createServer } from 'http';
-import { join } from 'path';
+const express = require('express');
+const http = require('http');
+const path = require('path');
 
 const config = { port: 3000 };
 
 const app = express();
 
 app.set('view options', { layout: false });
-app.use(static(`${__dirname}/template`));
+app.use(express.static(`${__dirname}/template`));
 
-app.use(favicon());
+app.use(express.favicon());
 
 if (app.get('env') === 'development') {
-  app.use(logger('dev'));
+  app.use(express.logger('dev'));
 } else {
-  app.use(logger('default'));
+  app.use(express.logger('default'));
 }
 
-app.use(bodyParser());
+app.use(express.bodyParser());
 
-app.use(cookieParser());
+app.use(express.cookieParser());
 
 app.use(app.router);
 
@@ -27,16 +27,16 @@ app.get('/', (req, res) => {
   res.render('/index.html');
 });
 
-app.use(static(join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((err, req, res, next) => {
   // NODE_ENV = 'production'
   if (app.get('env') === 'development') {
-    const errorHandler = _errorHandler();
+    const errorHandler = express.errorHandler();
     errorHandler(err, req, res, next);
   } else {
     res.send(500);
   }
 });
 
-createServer(app).listen(config.port, () => {});
+http.createServer(app).listen(config.port, () => {});
