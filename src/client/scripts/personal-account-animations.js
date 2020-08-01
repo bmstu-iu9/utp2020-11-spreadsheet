c709391c22bec7e9af396f2f2623d957f743a3f4
kurigohan <shehamane@mail.ru>
2020-08-01 23:45:47 +0300

    new book animation v1

---
+++ src/client/scripts/personal-account-animations.js
@@ -0,0 +1,9 @@
+const decorativeLine = document.getElementById('decorative-line');
+
+function hideDecorativeLine() {
+  decorativeLine.style.width = '210px';
+}
+
+function showDecorativeLine() {
+  decorativeLine.style.width = '100%';
+}
