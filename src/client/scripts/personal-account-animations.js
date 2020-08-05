b64a32faeb6b54b6d6904fd0b8df2a98095dd461
kurigohan <shehamane@mail.ru>
2020-08-05 12:57:49 +0300

    eslint fix

--- src/client/scripts/personal-account-animations.js
+++
@@ -1,9 +0,0 @@
-const decorativeLine = document.getElementById('decorative-line');
-
-function hideDecorativeLine() {
-  decorativeLine.style.width = '210px';
-}
-
-function showDecorativeLine() {
-  decorativeLine.style.width = '100%';
-}
