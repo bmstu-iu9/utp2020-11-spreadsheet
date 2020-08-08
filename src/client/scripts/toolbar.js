03145a19f4ec2a87bb76c73e1fe04ebcb8cc34a9
kurigohan <shehamane@mail.ru>
2020-08-08 14:01:01 +0300

    Revert "tabs-script"
    This reverts commit 7518076708c8176f68490f2ddc675fce075f26d4.

--- src/client/scripts/toolbar.js
+++ src/client/scripts/toolbar.js
@@ -1,12 +1,2 @@
-let toolbarTabs = document.querySelectorAll('div.toolbar-tab');
+let toolbarTabs = document.getElementById('toolbar-tabs');
 
-
-for (let toolbarTab of toolbarTabs) {
-  toolbarTab.addEventListener("click", e => {
-    e.preventDefault();
-
-    document.querySelector('div.toolbar-tab.toolbar-tab-active').classList.remove('toolbar-tab-active');
-    document.getElementById('toolbar-wrapper').children[1].classList.add('hide');
-
-  });
-}
