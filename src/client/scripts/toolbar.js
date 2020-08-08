7518076708c8176f68490f2ddc675fce075f26d4
kurigohan <shehamane@mail.ru>
2020-08-08 13:46:16 +0300

    tabs-script

--- src/client/scripts/toolbar.js
+++ src/client/scripts/toolbar.js
@@ -1,2 +1,12 @@
-let toolbarTabs = document.getElementById('toolbar-tabs');
+let toolbarTabs = document.querySelectorAll('div.toolbar-tab');
 
+
+for (let toolbarTab of toolbarTabs) {
+  toolbarTab.addEventListener("click", e => {
+    e.preventDefault();
+
+    document.querySelector('div.toolbar-tab.toolbar-tab-active').classList.remove('toolbar-tab-active');
+    document.getElementById('toolbar-wrapper').children[1].classList.add('hide');
+
+  });
+}
