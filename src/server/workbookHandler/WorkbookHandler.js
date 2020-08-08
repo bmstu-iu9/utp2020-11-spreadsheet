62981d0761a41f5a0da5d0a6e713c83bebdd954f
AlexisOMG <alesha-gramazeka@yandex.ru>
2020-08-09 01:45:09 +0300

    Done some refactoring

--- src/server/workbookHandler/WorkbookHandler.js
+++ src/server/workbookHandler/WorkbookHandler.js
@@ -3,16 +3,15 @@ import WorkbookModel from '../database/WorkbookModel.js';
 import FormatError from '../../lib/errors/FormatError.js';
 import ClassConverter from '../../lib/saveWorkbook/ClassConverter.js';
 import JsonConverter from '../../lib/readWorkbook/JsonConverter.js';
-import DataRepo from '../database/DataRepo.js';
+import { zeroID } from '../synchronization/synchronizer.js';
 
 export default class WorkbookHandler {
-  constructor(database) {
-    this.database = database;
-    this.dataRepo = new DataRepo(this.database);
+  constructor(dataRepo) {
+    this.dataRepo = dataRepo;
   }
 
   get(login) {
-    if (typeof login === 'undefined') {
+    if (login === undefined) {
       throw new FormatError('getting workbooks without login');
     }
     try {
@@ -20,7 +19,7 @@ export default class WorkbookHandler {
       const result = [];
       list.forEach((wbModel) => {
         const workbook = { id: wbModel.id };
-        const reads = ClassConverter.convertToJson(JsonConverter.readWorkbook(wbModel.path));
+        const reads = ClassConverter.readObject(JsonConverter.readWorkbook(wbModel.path));
         workbook.name = reads.name;
         workbook.sheets = reads.sheets;
         result.push(workbook);
@@ -35,11 +34,11 @@ export default class WorkbookHandler {
   }
 
   post(login, workbook, pathToWorkbooks) {
-    if (typeof login === 'undefined' || login === '') {
+    if (login === undefined || login === '') {
       throw new FormatError('creating workbook without login');
-    } else if (typeof workbook === 'undefined') {
+    } else if (workbook === undefined) {
       throw new FormatError('creating workbook without workbook');
-    } else if (typeof pathToWorkbooks === 'undefined') {
+    } else if (pathToWorkbooks === undefined || pathToWorkbooks === '') {
       throw new FormatError('creating workbook without path');
     }
     try {
@@ -50,9 +49,9 @@ export default class WorkbookHandler {
     try {
       ClassConverter.saveJson(workbook, pathToWorkbooks);
       const workbookModel = new WorkbookModel(`${pathToWorkbooks}/${workbook.name}.json`, login);
-      const book = ClassConverter.convertToJson(JsonConverter.readWorkbook(workbookModel.path));
+      const book = ClassConverter.readObject(JsonConverter.readWorkbook(workbookModel.path));
       const workbookID = { id: this.dataRepo.workbookRepo.save(workbookModel) };
-      workbookID.lastCommit = '00000000-0000-0000-0000-000000000000';
+      workbookID.lastCommit = zeroID;
       workbookID.name = book.name;
       workbookID.sheets = book.sheets;
       return {
@@ -65,6 +64,11 @@ export default class WorkbookHandler {
   }
 
   delete(login, workbookID) {
+    if (login === undefined) {
+      throw new FormatError('deleting workbooks without login');
+    } else if (workbookID === undefined) {
+      throw new FormatError('deleting workbooks without book id');
+    }
     try {
       this.dataRepo.tokenRepo.getByLogin(login);
     } catch (error) {
