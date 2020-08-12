c3a7e07a53ddedbbccc69574ab03681a27dafa63
Artyom <flygrounder@tutanota.com>
2020-08-12 12:25:02 +0300

    Added basic version of GET  /workbook/{id}

---
+++ src/server/handlers/WorkbookIdHandler.js
@@ -0,0 +1,20 @@
+import EndpointHandler from './EndpointHandler.js';
+import JsonConverter from '../../lib/readWorkbook/JsonConverter.js';
+
+export default class WorkbookIdHandler extends EndpointHandler {
+  get(req, res) {
+    if (req.user === undefined) {
+      res.sendStatus(401);
+    }
+    let workbook;
+    try {
+      workbook = this.dataRepo.workbookRepo.getById(req.params.id);
+    } catch (error) {
+      res.sendStatus(404);
+      return;
+    }
+    const content = JsonConverter.readWorkbook(workbook.path);
+    content.id = workbook.id;
+    res.status(200).json(content);
+  }
+}
---
+++ src/tests/server/handlers/WorkbookIdHandler.test.js
@@ -0,0 +1,69 @@
+import * as assert from 'assert';
+import express from 'express';
+import request from 'supertest';
+import mock from 'mock-fs';
+import Workbook from '../../../lib/spreadsheets/Workbook.js';
+import ClassConverter from '../../../lib/saveWorkbook/ClassConverter.js';
+import HeaderMatcher from '../../../server/authorization/HeaderMatcher.js';
+import TokenAuthencticator from '../../../server/authorization/TokenAuthenticator.js';
+import Authorizer from '../../../server/authorization/Authorizer.js';
+import TestEnvironment from '../database/TestEnvironment.js';
+import WorkbookIdHandler from '../../../server/handlers/WorkbookIdHandler.js';
+import WorkbookModel from '../../../server/database/WorkbookModel.js';
+
+describe('WorkbookIdHandler', () => {
+  let environment;
+  let app;
+
+  beforeEach(() => {
+    environment = TestEnvironment.getInstance();
+    const handler = new WorkbookIdHandler(environment.dataRepo);
+    const matcher = new HeaderMatcher('authorization', 'Token ');
+    const authenticator = new TokenAuthencticator(matcher, environment.dataRepo);
+    const authorizer = new Authorizer(authenticator);
+    app = express();
+    app.use(authorizer.getMiddleware());
+    app.get('/:id', (req, res) => handler.get(req, res));
+  });
+  afterEach(() => {
+    TestEnvironment.destroyInstance();
+  });
+
+  describe('get', () => {
+    it('should return 401 for unauthorized request', () => request(app)
+      .get('/1')
+      .expect(401));
+    it('should return 404 if workbook was not found', () => {
+      environment.init();
+      environment.addUsers(1, true);
+      const { token } = environment.userTokens[0];
+      return request(app)
+        .get('/1')
+        .set('Authorization', `Token ${token.uuid}`)
+        .expect(404);
+    });
+    it('should return a workbook', () => {
+      mock({
+        './test.json': '',
+      });
+      environment.init();
+      environment.addUsers(1, true);
+      const { username, token } = environment.userTokens[0];
+      const workbook = new Workbook('test');
+      ClassConverter.saveJson(workbook, './');
+      const workbookModel = new WorkbookModel('./test.json', username);
+      environment.dataRepo.workbookRepo.save(workbookModel);
+      return request(app)
+        .get('/1')
+        .set('Authorization', `Token ${token.uuid}`)
+        .expect(200)
+        .then((response) => {
+          assert.strictEqual(response.body.name, 'test');
+          assert.strictEqual(response.body.id, 1);
+        })
+        .finally(() => {
+          mock.restore();
+        });
+    });
+  });
+});
