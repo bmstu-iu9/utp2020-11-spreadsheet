8243d3f6f5a82a59a258abed7d9bad1529a312f4
Artyom <flygrounder@tutanota.com>
2020-08-17 11:52:11 +0300

    Added PATCH /workbook/{id}

--- src/server/handlers/WorkbookIdHandler.js
+++ src/server/handlers/WorkbookIdHandler.js
@@ -5,6 +5,9 @@ import WorkbookPathGenerator from '../save/WorkbookPathGenerator.js';
 import CommitLoader from '../save/CommitLoader.js';
 import CommitPathGenerator from '../save/CommitPathGenerator.js';
 import CommitFinder from '../synchronization/CommitFinder.js';
+import { Synchronizer } from '../synchronization/Synchronizer.js';
+import WorkbookSaver from '../save/WorkbookSaver.js';
+import CommitSaver from '../save/CommitSaver.js';
 
 export default class WorkbookIdHandler extends EndpointHandler {
   get(req, res) {
@@ -17,9 +20,47 @@ export default class WorkbookIdHandler extends EndpointHandler {
     }
   }
 
-  delete(req, res) {
-    const id = Number.parseInt(req.params.id, 10);
+  patch(req, res) {
     if (this.validateAccess(req, res)) {
+      const id = Number.parseInt(req.params.id, 10);
+      const workbook = this.fetchWorkbook(id);
+      const commits = this.fetchCommits(id);
+      const synchronizer = new Synchronizer(workbook, commits);
+      let conflicts;
+      try {
+        conflicts = synchronizer.addCommits(
+          req.body.changes,
+          req.body.lastSynchronizedCommit,
+        );
+      } catch {
+        res.sendStatus(400);
+        return;
+      }
+      if (conflicts.length !== 0) {
+        res.status(409).json(conflicts);
+        return;
+      }
+      this.saveCommits(synchronizer.acceptedCommits);
+      this.saveWorkbook(synchronizer.workbook);
+      res.sendStatus(200);
+    }
+  }
+
+  saveCommits(commits) {
+    const generator = new CommitPathGenerator(this.config.pathToCommits);
+    const saver = new CommitSaver(generator);
+    saver.save(1, commits);
+  }
+
+  saveWorkbook(workbook) {
+    const generator = new WorkbookPathGenerator(this.config.pathToWorkbooks);
+    const saver = new WorkbookSaver(generator);
+    saver.save(workbook, 1);
+  }
+
+  delete(req, res) {
+    if (this.validateAccess(req, res)) {
+      const id = Number.parseInt(req.params.id, 10);
       this.dataRepo.workbookRepo.delete(id);
       const generator = new WorkbookPathGenerator(this.config.pathToWorkbooks);
       const path = generator.generate(id);
@@ -50,12 +91,8 @@ export default class WorkbookIdHandler extends EndpointHandler {
 
   getWorkbook(req, res) {
     const id = Number.parseInt(req.params.id, 10);
-    const pathGenerator = new WorkbookPathGenerator(this.config.pathToWorkbooks);
-    const loader = new WorkbookLoader(pathGenerator);
-    let content;
-    try {
-      content = loader.load(id);
-    } catch {
+    const content = this.fetchWorkbook(id);
+    if (content === undefined) {
       res.sendStatus(404);
       return;
     }
@@ -65,12 +102,8 @@ export default class WorkbookIdHandler extends EndpointHandler {
 
   getCommits(req, res) {
     const id = Number.parseInt(req.params.id, 10);
-    const pathGenerator = new CommitPathGenerator(this.config.pathToCommits);
-    const loader = new CommitLoader(pathGenerator);
-    let commits;
-    try {
-      commits = loader.load(id);
-    } catch {
+    const commits = this.fetchCommits(id);
+    if (commits === undefined) {
       res.sendStatus(404);
       return;
     }
@@ -81,5 +114,27 @@ export default class WorkbookIdHandler extends EndpointHandler {
       return;
     }
     res.status(200).json(commits.slice(afterPosition + 1));
+  }
+
+  fetchWorkbook(id) {
+    const pathGenerator = new WorkbookPathGenerator(this.config.pathToWorkbooks);
+    const loader = new WorkbookLoader(pathGenerator);
+    return WorkbookIdHandler.fetchWithLoader(loader, id);
+  }
+
+  fetchCommits(id) {
+    const pathGenerator = new CommitPathGenerator(this.config.pathToCommits);
+    const loader = new CommitLoader(pathGenerator);
+    return WorkbookIdHandler.fetchWithLoader(loader, id);
+  }
+
+  static fetchWithLoader(loader, id) {
+    let content;
+    try {
+      content = loader.load(id);
+    } catch {
+      return undefined;
+    }
+    return content;
   }
 }
--- src/tests/server/handlers/WorkbookIdHandler.test.js
+++ src/tests/server/handlers/WorkbookIdHandler.test.js
@@ -15,6 +15,9 @@ import WorkbookModel from '../../../server/database/WorkbookModel.js';
 import WorkbookSaver from '../../../server/save/WorkbookSaver.js';
 import WorkbookPathGenerator from '../../../server/save/WorkbookPathGenerator.js';
 import CommitPathGenerator from '../../../server/save/CommitPathGenerator.js';
+import Spreadsheet from '../../../lib/spreadsheets/Spreadsheet.js';
+import CommitLoader from '../../../server/save/CommitLoader.js';
+import WorkbookLoader from '../../../server/save/WorkbookLoader.js';
 
 describe('WorkbookIdHandler', () => {
   let environment;
@@ -23,26 +26,50 @@ describe('WorkbookIdHandler', () => {
   const createWorkbook = () => {
     environment.addUsers(1, true);
     const { username } = environment.userTokens[0];
-    const workbook = new Workbook('test');
-    const generator = new WorkbookPathGenerator('.');
+    const workbook = new Workbook('test', [new Spreadsheet('test')]);
+    const generator = new WorkbookPathGenerator('workbooks');
     const saver = new WorkbookSaver(generator);
     saver.save(workbook, 1);
     const workbookModel = new WorkbookModel(username);
     environment.dataRepo.workbookRepo.save(workbookModel);
   };
 
+  const createCommits = () => {
+    const commits = [{ ID: zeroID }];
+    const commitPathGenerator = new CommitPathGenerator('commits');
+    const commitSaver = new CommitSaver(commitPathGenerator);
+    commitSaver.save(1, commits);
+  };
+
   beforeEach(() => {
     environment = TestEnvironment.getInstance();
     environment.init();
     const handler = new WorkbookIdHandler(environment.dataRepo, {
-      pathToWorkbooks: '.',
-      pathToCommits: '.',
+      pathToWorkbooks: 'workbooks',
+      pathToCommits: 'commits',
     });
     const matcher = new HeaderMatcher('authorization', 'Token ');
     const authenticator = new TokenAuthencticator(matcher, environment.dataRepo);
     const authorizer = new Authorizer(authenticator);
     app = express();
     app.use(authorizer.getMiddleware());
+
+    // express.json() does not work correctly with mock-fs
+    app.use((req, res, next) => {
+      if (req.method !== 'PATCH') {
+        next();
+        return;
+      }
+      let data = '';
+      req.on('data', (chunk) => {
+        data += chunk;
+      });
+      req.on('end', () => {
+        req.body = JSON.parse(data);
+        next();
+      });
+    });
+    app.patch('/:id', (req, res) => handler.patch(req, res));
     app.get('/:id', (req, res) => handler.get(req, res));
     app.delete('/:id', (req, res) => handler.delete(req, res));
   });
@@ -64,10 +91,12 @@ describe('WorkbookIdHandler', () => {
     });
     it('should return 404 for absent workbook file', () => {
       mock({
-        '1.json': '',
+        workbooks: {
+          '1.json': '',
+        },
       });
       createWorkbook();
-      fs.unlinkSync('1.json');
+      fs.unlinkSync('workbooks/1.json');
       const { token } = environment.userTokens[0];
       return request(app)
         .get('/1')
@@ -77,7 +106,9 @@ describe('WorkbookIdHandler', () => {
     });
     it('should return a workbook', () => {
       mock({
-        './1.json': '',
+        workbooks: {
+          '1.json': '',
+        },
       });
       createWorkbook();
       const { token } = environment.userTokens[0];
@@ -89,24 +120,33 @@ describe('WorkbookIdHandler', () => {
           assert.strictEqual(response.body.name, 'test');
           assert.strictEqual(response.body.id, 1);
         })
-        .finally(() => {
-          mock.restore();
-        });
+        .then(mock.restore);
     });
     it('should return 403', () => {
+      mock({
+        workbooks: {
+          '1.json': '',
+        },
+      });
       createWorkbook();
       environment.addUsers(1, true);
       const { token } = environment.userTokens[1];
       return request(app)
         .get('/1')
         .set('Authorization', `Token ${token.uuid}`)
-        .expect(403);
+        .expect(403)
+        .then(mock.restore);
     });
     it('should return commits', () => {
-      createWorkbook();
       mock({
-        '1.commits.json': '',
+        workbooks: {
+          '1.json': '',
+        },
+        commits: {
+          '1.commits.json': '',
+        },
       });
+      createWorkbook();
       const commits = [
         {
           ID: zeroID,
@@ -116,7 +156,7 @@ describe('WorkbookIdHandler', () => {
         },
       ];
       const { token } = environment.userTokens[0];
-      const generator = new CommitPathGenerator('.');
+      const generator = new CommitPathGenerator('commits');
       const saver = new CommitSaver(generator);
       saver.save(1, commits);
       return request(app)
@@ -126,22 +166,31 @@ describe('WorkbookIdHandler', () => {
         .then((response) => {
           assert.deepStrictEqual(response.body, commits.slice(1));
         })
-        .finally(() => {
-          mock.restore();
-        });
+        .then(mock.restore);
     });
   });
   it('should return 404 for absent file', () => {
+    mock({
+      workbooks: {
+        '1.json': '',
+      },
+    });
     createWorkbook();
     const { token } = environment.userTokens[0];
     return request(app)
       .get(`/1?after=${zeroID}`)
       .set('Authorization', `Token ${token.uuid}`)
-      .expect(404);
+      .expect(404)
+      .then(mock.restore);
   });
   it('should return 409 for absent commit', () => {
     mock({
-      '1.commits.json': JSON.stringify([{ ID: zeroID }]),
+      workbooks: {
+        '1.json': '',
+      },
+      commits: {
+        '1.commits.json': JSON.stringify([{ ID: zeroID }]),
+      },
     });
     createWorkbook();
     const { token } = environment.userTokens[0];
@@ -149,9 +198,7 @@ describe('WorkbookIdHandler', () => {
       .get('/1?after=6fdc5457-d4bc-4e3e-81ba-bc9f3b49ba7b')
       .set('Authorization', `Token ${token.uuid}`)
       .expect(409)
-      .then(() => {
-        mock.restore();
-      });
+      .then(mock.restore);
   });
   describe('#delete()', () => {
     it('should give response 401 for unauthorized user', (done) => {
@@ -176,18 +223,119 @@ describe('WorkbookIdHandler', () => {
         .set('Authorization', `Token ${environment.userTokens[1].token.uuid}`)
         .expect(403, done);
     });
-    it('should give response 200 for successful deletion', (done) => {
-      environment.addUsers(1, true);
-      const workbookModel = new WorkbookModel('test0');
-      const id = environment.dataRepo.workbookRepo.save(workbookModel);
-      const generator = new WorkbookPathGenerator('.');
-      const saver = new WorkbookSaver(generator);
-      saver.save(new Workbook('test'), id);
+    it('should give response 200 for successful deletion', () => {
+      mock({
+        workbooks: {
+          '1.json': '',
+        },
+      });
+      createWorkbook();
       const { token } = environment.userTokens[0];
-      request(app)
-        .delete(`/${id.toString()}`)
+      return request(app)
+        .delete('/1')
         .set('Authorization', `Token ${token.uuid}`)
-        .expect(200, done);
+        .expect(200)
+        .then(mock.restore);
+    });
+  });
+  describe('#patch', () => {
+    const commits = {
+      lastSynchronizedCommit: zeroID,
+      changes: [{
+        ID: 'cf3c1cf6-be2f-4a6a-b69b-97b9c1126065',
+        changeType: 'color',
+        cellAddress: 'A1',
+        color: '#aaaaaa',
+        page: 0,
+      }],
+    };
+
+    it('should return 401 for unauthorized request', () => {
+      mock({
+        workbooks: {
+          '1.json': '',
+        },
+      });
+      createWorkbook();
+      return request(app).patch('/1').send({}).expect(401)
+        .then(mock.restore);
+    });
+    it('should save commits and return 200', () => {
+      mock({
+        workbooks: {
+          '1.json': '',
+        },
+        commits: {
+          '1.commits.json': '',
+        },
+      });
+      createWorkbook();
+      createCommits();
+      const { token } = environment.userTokens[0];
+      return request(app)
+        .patch('/1')
+        .set('Authorization', `Token ${token.uuid}`)
+        .send(JSON.stringify(commits))
+        .expect(200)
+        .then(() => {
+          const generator = new CommitPathGenerator('commits');
+          const loader = new CommitLoader(generator);
+          const actualCommits = loader.load(1);
+          assert.deepStrictEqual(actualCommits[1], commits.changes[0]);
+        })
+        .then(() => {
+          const generator = new WorkbookPathGenerator('workbooks');
+          const loader = new WorkbookLoader(generator);
+          const workbook = loader.load(1);
+          assert.strictEqual(workbook.spreadsheets[0].cells.get('A1').color, '#aaaaaa');
+        })
+        .then(mock.restore);
+    });
+    it('should return 400 for incorrect commits', () => {
+      mock({
+        workbooks: {
+          '1.json': '',
+        },
+        commits: {
+          '1.commits.json': '',
+        },
+      });
+      createWorkbook();
+      createCommits();
+      const { token } = environment.userTokens[0];
+      return request(app)
+        .patch('/1')
+        .set('Authorization', `Token ${token.uuid}`)
+        .send('{}')
+        .expect(400)
+        .then(mock.restore);
+    });
+    it('should return 409 for conflicting commits', () => {
+      mock({
+        workbooks: {
+          '1.json': '',
+        },
+        commits: {
+          '1.commits.json': '',
+        },
+      });
+      createWorkbook();
+      const generator = new CommitPathGenerator('commits');
+      const saver = new CommitSaver(generator);
+      saver.save(1, [
+        { ID: zeroID },
+        ...commits.changes,
+      ]);
+      const { token } = environment.userTokens[0];
+      return request(app)
+        .patch('/1')
+        .set('Authorization', `Token ${token.uuid}`)
+        .send(JSON.stringify(commits))
+        .expect(409)
+        .then((res) => {
+          assert.deepStrictEqual(res.body, commits.changes);
+        })
+        .then(mock.restore);
     });
   });
 });
