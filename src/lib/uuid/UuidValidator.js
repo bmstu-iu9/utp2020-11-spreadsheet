8f15e9becbe36d16abaf31c73636b87a45df4a77
Artyom Belousov <flygrounder@tutanota.com>
2020-08-17 17:45:52 +0000

    Update src/lib/uuid/UuidValidator.js
    Co-authored-by: MKondakova <46626161+MKondakova@users.noreply.github.com>

--- src/lib/uuid/UuidValidator.js
+++ src/lib/uuid/UuidValidator.js
@@ -1,7 +1,7 @@
 export default class UuidValidator {
   static isUuidValid(uuid) {
     const regExp = new RegExp(
-      '^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$',
+      '^[a-fA-F0-9]{8}-([a-fA-F0-9]{4}-){3}[a-fA-F0-9]{12}$',
     );
     return regExp.test(uuid);
   }
