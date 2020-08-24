85892d0ef2f56b9409ba30878de3fe5256fb278d
MKondakova <milanon@ya.ru>
2020-08-24 12:33:44 +0300

    Add PostUserRequest

---
+++ src/tests/client/requests/PostUserRequest.test.js
@@ -0,0 +1,41 @@
+import sinon from 'sinon';
+import * as assert from 'assert';
+import RequestAuthorizer from '../../../client/js/requests/RequestAuthorizer.js';
+import Request from '../../../client/js/requests/Request.js';
+import PostUserRequest from '../../../client/js/requests/PostUserRequest.js';
+import UserModel from '../../../server/database/UserModel.js';
+
+describe('PostUserRequest', () => {
+  const baseUrl = '/';
+  const authorizer = new RequestAuthorizer('647d1546-fab5-49a8-8f89-3cdebec4b8be');
+  const request = new PostUserRequest(baseUrl, authorizer);
+
+  before(() => {
+    global.XMLHttpRequest = sinon.useFakeXMLHttpRequest();
+  });
+  after(() => {
+    global.XMLHttpRequest.restore();
+  });
+
+  it('should extend Request', () => {
+    assert.strictEqual(request instanceof Request, true);
+  });
+
+  describe('#send', () => {
+    it('should register new user', () => {
+      const username = 'username';
+      const password = 'password';
+      global.XMLHttpRequest.onCreate = (req) => {
+        req.send = (data) => {
+          const parsed = JSON.parse(data);
+          assert.deepStrictEqual(parsed, { password, username });
+          req.respond(200,
+            { 'Content-Type': 'application/json' },
+            JSON.stringify({ isAdmin: false, username }));
+        };
+      };
+      const result = request.send(username, password);
+      assert.deepStrictEqual(result, new UserModel(username, false));
+    });
+  });
+});
